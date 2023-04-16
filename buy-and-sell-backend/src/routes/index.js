49ddb8acf971e8965dbc3871415a59587afb3e9c
Tom <pjoh1092@gmail.com>
2023-04-16 11:37:51 -0500

    initial commit

---
+++ buy-and-sell-backend/src/routes/index.js
@@ -0,0 +1,18 @@
+import { addViewToListingRoute } from "./addViewToListing";
+import { getAllListingsRoute } from "./getAllListings";
+import { getListingRoute } from "./getListing";
+import { getUserListingsRoute } from "./getUserListings";
+import { createNewListingRoute } from "./createNewListing";
+import { updateListingRoute } from "./updateListing";
+import { deleteListingRoute } from "./deleteListing";
+
+export default [
+    addViewToListingRoute,
+    getAllListingsRoute,
+    getListingRoute,
+    getUserListingsRoute,
+    createNewListingRoute,
+    updateListingRoute,
+    deleteListingRoute,
+
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:false,
+    firebase:{
+        apiKey: "AIzaSyDFNjfWeikkgHJpAJCzpePzmzBSLeGpshk",
+        authDomain: "buy-and-sell-10d29.firebaseapp.com",
+        projectId: "buy-and-sell-10d29",
+        storageBucket: "buy-and-sell-10d29.appspot.com",
+        messagingSenderId: "924012747843",
+        appId: "1:924012747843:web:99f2c444ba52eda8d888f6",
+        measurementId: "G-2S3MDS0KTN"
+      }
+};
